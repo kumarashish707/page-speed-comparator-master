a37df4fe361c62c869ec65b6ff443b1d80f9c1e6
kumarashish707 <ashishsharma4750@gmail.com>
2023-06-29 11:04:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './componenets/header/header';
+import URLFormComponent from './componenets/urlField/URLFormComponent';
+
+function App() {
+	return (
+		<>
+			<Header />
+			<URLFormComponent />
+		</>
+	);
+}
+
+export default App;
